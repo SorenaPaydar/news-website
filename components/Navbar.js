c44ec5c47b19be5b4024ebfd25ca1e02998e8ba6
Mohamad Talei <mohamadtalei02@gmail.com>
2021-07-19 00:25:07 -0700

    Add navbar

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+    return ( 
+        <div className={styles.navbar}>
+            <h2 className={styles.title}>Title</h2>
+            <div className={styles.links}>
+                <a>Home</a>
+                <a>Article</a>
+                <a>Contact</a>
+                <a>Purchase</a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
