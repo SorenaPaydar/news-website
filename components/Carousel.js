7bac98a6d7791bbcc34c2048bc4eb3775a46efa2
Mohamad Talei <mohamadtalei02@gmail.com>
2021-07-20 14:25:55 -0700

    Few Changes In Navbar Styles

---
+++ components/Carousel.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Carousel.module.css';
+
+
+const Carousel = () => {
+    return ( 
+        <div className={styles.carousel}>
+
+        </div>
+     );
+}
+ 
+export default Carousel
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="home">
-      
-    </div>
-  )
+    <div className={styles.home}>
+    </div> 
+    );
+
 }
+ 
+export default Home;
