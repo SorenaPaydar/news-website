12cdef64d1e38194e80d41af15b63960f3ad5b31
mohamadtalei <85223734+mohamadtalei@users.noreply.github.com>
2021-07-24 04:46:56 -0700

    Update Carousel.js

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,51 +1,52 @@
 import styles from '../styles/Carousel.module.scss';
 import { useEffect, useState } from 'react';
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import { AiOutlineLeft,AiOutlineRight } from "react-icons/ai";
 
-const Carousel = ({ headingsRes }) => {
+
+
+const Carousel = ({headingsRes}) => {
     const headings = headingsRes.articles;
-    const [indx, setIndx] = useState(0);
+    const [indx, setindx] = useState(0);
     const length = headings.length;
-    const next = () => {
-        if (indx === length - 1) {
-            setIndx(0);
+
+    const next = ()=>{
+        if(indx===length-1){
+            setindx(0);
         }
-        else {
-            setIndx((p) => p + 1);
-        }
+        else{
+            setindx((p)=>p+1);
+        }   
     }
-    const pre = () => {
-        if (indx === 0) {
-            setIndx(length - 1);
+    const pre = ()=>{
+        if(indx===0){
+            setindx(length-1);
         }
-        else {
-            setIndx((p) => p - 1);
-        }
+        else{
+            setindx((p)=>p-1);
+        }   
     }
-    const [timer, setTimer] = useState(null)
+    const [timer,settimer]=useState(null)
     useEffect(() => {
         clearInterval(timer);
-        setTimer(null);
-        setTimer(setInterval(next, 10000));
-    }, [indx]);
-
-    return (
+        settimer(null);
+        settimer(setInterval(next,10000));
+    },[indx]);
+    
+    return ( 
         <div className={styles.sliderContainer}>
-            <button onClick={pre} className={styles.leftButton}><AiOutlineLeft /></button>
-            <button onClick={next} className={styles.rightButton}><AiOutlineRight /></button>
-            <div className={styles.slide} style={{ 'left': `-${100 * indx}vw` }} >
+            <button onClick={pre} className={styles.leftButton}><AiOutlineLeft/></button>
+            <button onClick={next} className={styles.rightButton}><AiOutlineRight/></button>
+            <div className={styles.slide} style={{'left':`-${100*indx}vw`}}>
 
-                {
-                    headings.map((slide, index) =>
-                        <div className={indx === index ? styles.activeSlide : styles.slidePic}>
-                            <img src={slide.urlToImage} />
-                            <div className={styles.headerTitle}>{slide.title}</div>
-                        </div>
-                    )
-                }
+            {headings.map((slide,index)=>
+                <div className={indx===index?styles.activeSlide:styles.slidePic}>
+                    <img src={slide.urlToImage} />
+                    <div className={styles.headerTitle}><a target='_blank' href={slide.url}>{slide.title}</a></div>
+                </div>
+            )}
             </div>
         </div>
-    );
+     );
 }
 
 
